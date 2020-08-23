afddb638731753f65614cbf15bbbf5e3eb55aca7
Wasim Raja <Wasim.Raja@csiro.au>
2020-08-23 22:02:36 +0800

    Make remUVcont.py python3 compatible.

--- bin/remUVcont.py
+++ bin/remUVcont.py
@@ -66,16 +66,16 @@ if __name__ == "__main__":
     n_left = args.n_left 
 
     if n_left > n_right: 
-	# Use all data to fit
-	n_left = 0
-	n_right = 0
+        # Use all data to fit
+        n_left = 0
+        n_right = 0
     if n_left < 0: 
-	# Start from the first data point on the left
-	n_left = 0
+        # Start from the first data point on the left
+        n_left = 0
     if n_right < 0: 
-	# Use all data for fitting:
-	n_left = 0
-	n_right = 0
+        # Use all data for fitting:
+        n_left = 0
+        n_right = 0
 
 
     nTaper = args.n_Taper #15 
@@ -87,20 +87,26 @@ if __name__ == "__main__":
     if overWrite == True:
             td = table(msData, readonly=False,ack=False)
     else:
-	    td = table(msData, readonly=True,ack=False)
-	    inTabDir,inTabName = os.path.split(msData)
-	    index = inTabName.index('.ms')
-	    outTable = inTabDir +  '/' + inTabName[0:index] + ".uvsub" + ".ms"
-	    print ("Copying  input  ms data to: %s \n" % (outTable))
-	    td.copy(outTable, deep=True, valuecopy=True, dminfo={}, endian='aipsrc', memorytable=False, copynorows=False)
-	    print ("Done copying...")
+            td = table(msData, readonly=True,ack=False)
+            inTabDir,inTabName = os.path.split(msData)
+            print("Table path: %s" % (inTabDir))
+            print("Table name: %s" % (inTabName))
+            if inTabName == "":
+                    index = inTabDir.index('.ms')
+                    outTable = inTabDir[0:index] + ".uvsub" + ".ms"
+            else:
+                    index = inTabName.index('.ms')
+                    outTable = inTabDir +  '/' + inTabName[0:index] + ".uvsub" + ".ms"
+            print ("Copying  input  ms data to: %s \n" % (outTable))
+            td.copy(outTable, deep=True, valuecopy=True, dminfo={}, endian='aipsrc', memorytable=False, copynorows=False)
+            print ("Done copying...")
 
-	    ts = table(outTable, readonly=False, ack=True)
+            ts = table(outTable, readonly=False, ack=True)
 
     ta = table("%s/ANTENNA"%(msData),readonly=True,ack=False)
     antNames = ta.getcol("NAME")
     nAnt = len(antNames)
-    nBase = nAnt * (nAnt + 1) / 2
+    nBase = nAnt * (nAnt + 1) // 2
     f1 = td.getcol('FLAG',startrow=0,nrow=1,rowincr=1) 
     time = td.getcol('TIME',startrow=0,nrow=-1,rowincr=1)
     nChan = f1.shape[1]
@@ -124,11 +130,11 @@ if __name__ == "__main__":
 
         nSampPerFit = 54*fit_54 #nchan # nchan
         nStagger = nSampPerFit 
-        n_win = nChan/nSampPerFit
-        print "The fitting will be done in 54 x",fit_54," Channel beam-forming intervals."
+        n_win = nChan//nSampPerFit
+        print("The fitting will be done in 54 x",fit_54," Channel beam-forming intervals.")
     else: 
-        nSampPerFit = nChan/n_win 
-        nStagger= nSampPerFit/2 
+        nSampPerFit = nChan//n_win 
+        nStagger= nSampPerFit//2 
 
     # Compute the flagging statistics of all channels in time-baseline space: 
     nBadChan_AllBaseAllTime = 0 
@@ -155,64 +161,64 @@ if __name__ == "__main__":
         a2 = td.getcol('ANTENNA2',startrow=irow,nrow=nBase,rowincr=1) 
 
         nBadChan_AllBase = 0 
-	#++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-	# Find channels that stand out as outliers when all 
-	# baselines are averaged for this time: 
-	# 1. get the baseline-averaged spectra
-	# 2. Detetct  outlier channels (could be RFI/Lines
-	# 3. Update the flagArray to further exclude these points 
-	#    from the fit.
-	ave_x = np.zeros((nChan),dtype="complex")
-	ave_y = np.zeros((nChan),dtype="complex")
-	ave_ispec = np.zeros((nChan),dtype="float32")
-	sumFlag_x = np.zeros((nChan),dtype="float32")
-	sumFlag_y = np.zeros((nChan),dtype="float32")
-	flagArr = np.ones((nChan),dtype="float32")
-	for jbase in range(0,nBase):
-	    if a1[jbase] != a2[jbase]:
-		# For xx
+        #++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        # Find channels that stand out as outliers when all 
+        # baselines are averaged for this time: 
+        # 1. get the baseline-averaged spectra
+        # 2. Detetct  outlier channels (could be RFI/Lines
+        # 3. Update the flagArray to further exclude these points 
+        #    from the fit.
+        ave_x = np.zeros((nChan),dtype="complex")
+        ave_y = np.zeros((nChan),dtype="complex")
+        ave_ispec = np.zeros((nChan),dtype="float32")
+        sumFlag_x = np.zeros((nChan),dtype="float32")
+        sumFlag_y = np.zeros((nChan),dtype="float32")
+        flagArr = np.ones((nChan),dtype="float32")
+        for jbase in range(0,nBase):
+            if a1[jbase] != a2[jbase]:
+                # For xx
                 flagInv =  np.array(np.logical_not(f[jbase,:,0]).astype(dtype="float32",casting='same_kind'))  
                 #ave_x = np.array(ave_x + np.multiply(np.abs(v[jbase,:,0]), flagInv))
                 ave_x = np.array(ave_x + np.multiply(v[jbase,:,0], flagInv))
-		# Sum the flag spectra (count of good points)
-		sumFlag_x = np.array(sumFlag_x + flagInv)
-		#
-		# Now for yy
+                # Sum the flag spectra (count of good points)
+                sumFlag_x = np.array(sumFlag_x + flagInv)
+                #
+                # Now for yy
                 flagInv =  np.array(np.logical_not(f[jbase,:,3]).astype(dtype="float32",casting='same_kind'))  
                 #ave_y = np.array(ave_y + np.multiply(np.abs(v[jbase,:,3]), flagInv))
                 ave_y = np.array(ave_y + np.multiply(v[jbase,:,3], flagInv))
-		# Sum the flag spectra (count of good points)
-		sumFlag_y = np.array(sumFlag_y + flagInv)
-	# Get the average spectra & avoid Inf:
-	for jchan in range(0,nChan):
-	    if sumFlag_x[jchan] > 0 and sumFlag_y[jchan] > 0:
-		ave_x[jchan] = ave_x[jchan]/sumFlag_x[jchan]
-		ave_y[jchan] = ave_y[jchan]/sumFlag_y[jchan]
-		flagInv[jchan] = 1.0
-	    else:
-		ave_x[jchan] = 0.0
-		ave_y[jchan] = 0.0
-		flagInv[jchan] = 0.0
-	    ave_ispec[jchan] = np.abs(ave_x[jchan] + ave_y[jchan]) 
-	# Derive outlier channels:
+                # Sum the flag spectra (count of good points)
+                sumFlag_y = np.array(sumFlag_y + flagInv)
+        # Get the average spectra & avoid Inf:
+        for jchan in range(0,nChan):
+            if sumFlag_x[jchan] > 0 and sumFlag_y[jchan] > 0:
+                ave_x[jchan] = ave_x[jchan]/sumFlag_x[jchan]
+                ave_y[jchan] = ave_y[jchan]/sumFlag_y[jchan]
+                flagInv[jchan] = 1.0
+            else:
+                ave_x[jchan] = 0.0
+                ave_y[jchan] = 0.0
+                flagInv[jchan] = 0.0
+            ave_ispec[jchan] = np.abs(ave_x[jchan] + ave_y[jchan]) 
+        # Derive outlier channels:
         ave_ispec_fit = pbp.process_bptab(inarr=ave_ispec,flagarr=flagInv,maskval=0.0,npts=nChan,nsampperfit=nSampPerFit,nstagger=nStagger,npoly=n_p,nharm=n_h,refant=0,nskipleft=n_left,nskipright=n_right) 
-	resi = np.array(ave_ispec - ave_ispec_fit)
-	sigma,mu = musigma.meanrms(a=resi,np=nChan)
-	thresh = 1.2
-	jcnt = 0
-	for jchan in range(0,nChan):
-	    #if np.abs(resi[jchan] - mu) > thresh*sigma :
-	    if np.abs(resi[jchan]) > thresh*sigma :
-		flagArr[jchan] = 0.0
-		jcnt = jcnt  + 1
-		#print ("# Excluded Channel: %6d , jchan/54: %f " % (jchan,jchan/54.0))
-	    else: 
-		flagArr[jchan] = 1.0
-	print ("# Excluded: %6d channels for Integration Number: %5d, MJD: %f " % (jcnt,itime,time[irow]/86400.0))
-	print ("# sigma: %f, mu: %f " % (sigma,mu))
-	#print ("# flagInv.shape: ",flagInv.shape)
-	#print ("# flagArr.shape: ",flagArr.shape)
-	#++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        resi = np.array(ave_ispec - ave_ispec_fit)
+        sigma,mu = musigma.meanrms(a=resi,np=nChan)
+        thresh = 1.2
+        jcnt = 0
+        for jchan in range(0,nChan):
+            #if np.abs(resi[jchan] - mu) > thresh*sigma :
+            if np.abs(resi[jchan]) > thresh*sigma :
+                flagArr[jchan] = 0.0
+                jcnt = jcnt  + 1
+                #print ("# Excluded Channel: %6d , jchan/54: %f " % (jchan,jchan/54.0))
+            else: 
+                flagArr[jchan] = 1.0
+        print ("# Excluded: %6d channels for Integration Number: %5d, MJD: %f " % (jcnt,itime,time[irow]/86400.0))
+        print ("# sigma: %f, mu: %f " % (sigma,mu))
+        #print ("# flagInv.shape: ",flagInv.shape)
+        #print ("# flagArr.shape: ",flagArr.shape)
+        #++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
         brow = itime 
@@ -230,8 +236,8 @@ if __name__ == "__main__":
                 flagMS = np.array(f[ibase,:,0].astype(dtype="float32",casting='same_kind'))   
                 #flagInv =  np.array(np.logical_not(f[ibase,:,0]).astype(dtype="float32",casting='same_kind'))  
                 flagInv =  np.multiply(flagArr,np.array(np.logical_not(f[ibase,:,0]).astype(dtype="float32",casting='same_kind')))
-	        #print ("# flagInv.shape Now: ",flagInv.shape)
-	        #print ("# flagArr.shape Now: ",flagArr.shape)
+                #print ("# flagInv.shape Now: ",flagInv.shape)
+                #print ("# flagArr.shape Now: ",flagArr.shape)
         
                 xrtmp = np.multiply(v[ibase,:,0].real, flagInv)  
                 xr = fi.finterp(inarr=xrtmp,flagarr=flagInv,maskval=0.0,npts=nChan,ntaper=nTaper,niter=nIter,refant=0)
@@ -248,8 +254,8 @@ if __name__ == "__main__":
             
                 #xr_resi = np.array(xr - xr_fit) 
                 #xi_resi = np.array(xi - xi_fit)
-		xr_resi = np.array(v[ibase,:,0].real - xr_fit) 
-		xi_resi = np.array(v[ibase,:,0].imag - xi_fit)
+                xr_resi = np.array(v[ibase,:,0].real - xr_fit) 
+                xi_resi = np.array(v[ibase,:,0].imag - xi_fit)
             
                 v_sub[ibase,:,0] = np.array(xr_resi + 1j*xi_resi) 
         
@@ -272,24 +278,24 @@ if __name__ == "__main__":
                 #v_sub[ibase,:,3] = (v[ibase,:,3].real - yr_fit) + 1j*(v[ibase,:,3].imag - yi_fit)
                 #yr_resi = np.array(yr - yr_fit)
                 #yi_resi = np.array(yi - yi_fit) 
-		yr_resi = np.array(v[ibase,:,3].real - yr_fit) 
-		yi_resi = np.array(v[ibase,:,3].imag - yi_fit)
+                yr_resi = np.array(v[ibase,:,3].real - yr_fit) 
+                yi_resi = np.array(v[ibase,:,3].imag - yi_fit)
             
                 v_sub[ibase,:,3] = np.array(yr_resi + 1j*yi_resi)
             
             irow =  irow + 1 
         ##
         # Update data in new table: 
-	if overWrite == True:
-		td.putcol('DATA', v_sub, startrow=irow-nBase, nrow=nBase, rowincr=1)
-	else:
-		ts.putcol('DATA', v_sub, startrow=irow-nBase, nrow=nBase, rowincr=1)
+        if overWrite == True:
+                td.putcol('DATA', v_sub, startrow=irow-nBase, nrow=nBase, rowincr=1)
+        else:
+                ts.putcol('DATA', v_sub, startrow=irow-nBase, nrow=nBase, rowincr=1)
     # Find out the total percentage of channels flagged for ALL baselines for ALL times:
     nTotVis = nChan*nBase*nRec
     if overWrite == True:
-	    td.flush(recursive=True)
+            td.flush(recursive=True)
             td.close()
     else:
             td.close()
-	    ts.flush(recursive=True)
+            ts.flush(recursive=True)
             ts.close()
